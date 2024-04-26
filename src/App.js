3b3ad8cf0d66172f542224b42363ebb7c11f8158
rizkydrma <rizky.darmarazak@gmail.com>
2024-04-26 17:04:55 +0700

    image compress & fix path header

--- src/App.js
+++ src/App.js
@@ -4,37 +4,29 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import ContactPage from 'pages/ContactPage';
 import ProductsPage from 'pages/ProductsPage';
 import ProductsDetailPage from 'pages/ProductDetailPage';
-import LoginPage from 'pages/LoginPage';
-import DashboardPage from 'pages/DashboardPage';
 
 function App() {
-  return (
-    <>
-      <Router>
-        <Route exact path="/">
-          <LoginPage />
-        </Route>
-        <Route path="/home">
-          <LandingPage />
-        </Route>
-        <Route path="/dashboard">
-          <DashboardPage />
-        </Route>
-        <Route path="/about">
-          <AboutPage />
-        </Route>
-        <Route path="/products">
-          <ProductsPage />
-        </Route>
-        <Route path="/product/:id">
-          <ProductsDetailPage />
-        </Route>
-        <Route path="/contact">
-          <ContactPage />
-        </Route>
-      </Router>
-    </>
-  );
+	return (
+		<>
+			<Router>
+				<Route exact path="/">
+					<LandingPage />
+				</Route>
+				<Route path="/about">
+					<AboutPage />
+				</Route>
+				<Route path="/products">
+					<ProductsPage />
+				</Route>
+				<Route path="/product/:id">
+					<ProductsDetailPage />
+				</Route>
+				<Route path="/contact">
+					<ContactPage />
+				</Route>
+			</Router>
+		</>
+	);
 }
 
 export default App;
--- src/parts/Header.js
+++ src/parts/Header.js
@@ -5,57 +5,53 @@ import { motion } from 'framer-motion';
 import ImageLogoGreen from 'asset/images/logo_green.png';
 
 export default function Header() {
-  const [drawer, setDrawer] = useState(false);
+	const [drawer, setDrawer] = useState(false);
 
-  const onClick = () => {
-    document.body.classList.toggle('hidden');
-    setDrawer(!drawer);
-  };
+	const onClick = () => {
+		document.body.classList.toggle('hidden');
+		setDrawer(!drawer);
+	};
 
-  return (
-    <nav className="navbar">
-      <div className="navbar_expand">
-        <Button
-          type="link"
-          href="/"
-          className={`brand_logo ${drawer ? 'open' : 'close'}`}
-        >
-          <img src={ImageLogoGreen} alt="logo hijau" className="image_logo" />
-          <div className="brand_name">
-            karyamulti<span>sejatiwood.com</span>
-          </div>
-        </Button>
-        <div className="navbar_toggler" id="drawer" onClick={() => onClick()}>
-          <div className={`bar ${drawer ? 'close' : ''}`}></div>
-        </div>
-      </div>
-      <motion.ul
-        className={`navbar_nav ${drawer ? 'collapse' : ''}`}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 1, type: 'spring', stifness: 10 }}
-      >
-        <li className="nav_item">
-          <Button type="link" href="/home" className="nav_link">
-            Home
-          </Button>
-        </li>
-        <li className="nav_item">
-          <Button type="link" href="/about" className="nav_link">
-            About Us
-          </Button>
-        </li>
-        <li className="nav_item">
-          <Button type="link" href="/products" className="nav_link">
-            Products
-          </Button>
-        </li>
-        <li className="nav_item">
-          <Button type="link" href="/contact" className="nav_link">
-            Contact Us
-          </Button>
-        </li>
-      </motion.ul>
-    </nav>
-  );
+	return (
+		<nav className="navbar">
+			<div className="navbar_expand">
+				<Button type="link" href="/" className={`brand_logo ${drawer ? 'open' : 'close'}`}>
+					<img src={ImageLogoGreen} alt="logo hijau" className="image_logo" />
+					<div className="brand_name">
+						karyamulti<span>sejatiwood.com</span>
+					</div>
+				</Button>
+				<div className="navbar_toggler" id="drawer" onClick={() => onClick()}>
+					<div className={`bar ${drawer ? 'close' : ''}`}></div>
+				</div>
+			</div>
+			<motion.ul
+				className={`navbar_nav ${drawer ? 'collapse' : ''}`}
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1 }}
+				transition={{ duration: 1, type: 'spring', stifness: 10 }}
+			>
+				<li className="nav_item">
+					<Button type="link" href="/" className="nav_link">
+						Home
+					</Button>
+				</li>
+				<li className="nav_item">
+					<Button type="link" href="/about" className="nav_link">
+						About Us
+					</Button>
+				</li>
+				<li className="nav_item">
+					<Button type="link" href="/products" className="nav_link">
+						Products
+					</Button>
+				</li>
+				<li className="nav_item">
+					<Button type="link" href="/contact" className="nav_link">
+						Contact Us
+					</Button>
+				</li>
+			</motion.ul>
+		</nav>
+	);
 }
